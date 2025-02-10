a18756e60d6a742905c72b1af5764575089c830f
Roman Grinyov <w3lifer@gmail.com>
2025-02-10 03:17:13 +0300

    added `testCreateFolder()`

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -7,13 +7,24 @@ use w3lifer\Google\Drive;
 
 final class MainTest extends TestCase
 {
-    public function testUpload(): void
+    public function testUpload()
     {
         $googleDrive = new Drive([
             'pathToCredentials' => __DIR__ . '/_data/credentials.json',
             'pathToToken' => __DIR__ . '/_data/token.json',
         ]);
         $fileId = $googleDrive->upload(__DIR__ . '/_data/test.txt');
+        $this->assertEquals(33, strlen($fileId));
+        $this->assertStringStartsWith('1', $fileId);
+    }
+
+    public function testCreateFolder()
+    {
+        $googleDrive = new Drive([
+            'pathToCredentials' => __DIR__ . '/_data/credentials.json',
+            'pathToToken' => __DIR__ . '/_data/token.json',
+        ]);
+        $fileId = $googleDrive->createFolder('test');
         $this->assertEquals(33, strlen($fileId));
         $this->assertStringStartsWith('1', $fileId);
     }
